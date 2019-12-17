9fa8e9021989bffa7c48b07d221e263fd84d5fb5
Anirudh Kamath <anirudhkamath@Anirudhs-Air.fios-router.home>
2019-12-16 23:41:13 -0500

    please

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
-import json
-from flask import Flask, request, redirect, g, render_template, session
-from flask_session import Session
-import requests
-from song import Song
-from urllib.parse import quote
-import time
 import base64
+import json
+import os
+from urllib.parse import quote
 
-# Authentication Steps, paramaters, and responses are defined at https://developer.spotify.com/web-api/authorization-guide/
-# Visit this url to see all the steps, parameters, and expected response.
+# from flask_session import Session
+import requests
+from flask import Flask, request, redirect, render_template
 
+from song import Song
 
 app = Flask(__name__)
-sess = Session()
 
 app.secret_key = 'super secret s'
 app.config['SESSION_TYPE'] = 'filesystem'
-sess.init_app(app)
+
 #  Client Keys
 CLIENT_ID = "b5b1d37af53a40bbae71580c1c426782"
 CLIENT_SECRET = "aadb1b4acb334c059711a6759619096e"
@@ -49,8 +46,10 @@ auth_query_parameters = {
 global q
 q = Song()
 
+@app.route('/access')
 def get_access():
-    authorization_header = {"Authorization": "Bearer {}".format(session['response']['access_token'])}
+    access = json.load(open('response.json', 'r'))
+    authorization_header = {"Authorization": "Bearer {}".format(access['access_token'])}
 
     auth_str = bytes('{}:{}'.format(CLIENT_ID, CLIENT_SECRET), 'utf-8')
     b64_auth_str = base64.b64encode(auth_str).decode('utf-8')
@@ -58,38 +57,40 @@ def get_access():
     response = requests.post('https://accounts.spotify.com/api/token',
                              headers={'Authorization': f'Basic {b64_auth_str}'},
                              data={"grant_type": "refresh_token",
-                                   'refresh_token': session['response']['refresh_token']})
+                                   'refresh_token': access['refresh_token']})
 
     data = dict(json.loads(response.text))
     print(data)
     for key, val in data.items():
-        session['response'][key] = val
+        access[key] = val
 
-    print(session['response'])
+    print(access)
+    json.dump(access, open('response.json', 'w'))
     global q
     q.update(authorization_header)
-    return session['response']
+    return access
 
 @app.route('/')
 def index():
-    if 'response' not in session:
+    if 'response.json' not in os.listdir('.'):
         return redirect('/login')
 
     get_access()
 
-    authorization_header = {"Authorization": "Bearer {}".format(session['response']['access_token'])}
-    user_profile_api_endpoint = "{}/me".format(SPOTIFY_API_URL)
-    profile_response = requests.get(user_profile_api_endpoint, headers=authorization_header)
-    profile_data = json.loads(profile_response.text)
-
-    # Get user playlist data
-    playlist_api_endpoint = "{}/playlists".format(profile_data["href"])
-    playlists_response = requests.get(playlist_api_endpoint, headers=authorization_header)
-    playlist_data = json.loads(playlists_response.text)
-
-    # Combine profile and playlist data to display
-    display_arr = [profile_data] + playlist_data["items"]
-    return render_template("index.html", sorted_array=display_arr)
+    # access = json.load(open('response.json', 'r'))
+    # authorization_header = {"Authorization": "Bearer {}".format(access['access_token'])}
+    # user_profile_api_endpoint = "{}/me".format(SPOTIFY_API_URL)
+    # profile_response = requests.get(user_profile_api_endpoint, headers=authorization_header)
+    # profile_data = json.loads(profile_response.text)
+    #
+    # # Get user playlist data
+    # playlist_api_endpoint = "{}/playlists".format(profile_data["href"])
+    # playlists_response = requests.get(playlist_api_endpoint, headers=authorization_header)
+    # playlist_data = json.loads(playlists_response.text)
+    #
+    # # Combine profile and playlist data to display
+    # display_arr = [profile_data] + playlist_data["items"]
+    return render_template("index.html")
 
 @app.route("/login")
 def login():
@@ -100,9 +101,15 @@ def login():
 
 @app.route('/queue')
 def addsong():
+    if 'response.json' not in os.listdir('.'):
+        return redirect('/login')
+
+    access = get_access()
+    authorization_header = {"Authorization": "Bearer {}".format(access['access_token'])}
     song = request.args.get('song')
+
     global q
-    return q.add(song)
+    return q.add(song, authorization_header)
 
 @app.route("/callback")
 def callback():
@@ -118,14 +125,18 @@ def callback():
     post_request = requests.post(SPOTIFY_TOKEN_URL, data=code_payload)
 
     # Auth Step 5: Tokens are Returned to Application
-    response_data = json.loads(post_request.text)
-    access_token = response_data["access_token"]
-    refresh_token = response_data["refresh_token"]
-    token_type = response_data["token_type"]
-    expires_in = response_data["expires_in"]
-    session['response'] = response_data
+    access = json.loads(post_request.text)
 
-    return redirect('/')
+    json.dump(access, open('response.json', 'w'))
+
+    return access
+
+@app.route('/next')
+def next():
+    print('next called')
+    global q
+    q.next()
+    return 'Next called'
 
 if __name__ == "__main__":
-    app.run(debug=True, port=PORT)
+    app.run(debug=False, port=PORT, host='0.0.0.0')
--- song.py
+++ song.py
@@ -4,45 +4,80 @@ import os
 import threading
 import json
 
+'''
+TODO:
+
+- Localize all constants in one place
+- Add methods to refresh tokens
+'''
+
 class Song:
     def __init__(self):
-        self.q = []
-        self.updated = False
-        self.playing = False
+        self.q = 0
+        self.timer = threading.Timer(0, lambda: print('Timer started'))
 
-    def add(self, song):
+    def queue(self):
+        playback_data = json.loads(requests.get('https://api.spotify.com/v1/me/player', headers=self.header).text)
+
+        self.wait((playback_data['item']['duration_ms'] - playback_data['progress_ms']) // 1000)
+
+        print(playback_data)
+        return playback_data
+
+    def add(self, song, header):
+        self.q += 1
+
         params = (
             ('uris', song),
         )
 
-        response = requests.post('https://api.spotify.com/v1/playlists/2CbUu1gLjkiPb1OnEvfvt8/tracks', headers=self.header,
+        response = requests.post('https://api.spotify.com/v1/playlists/2CbUu1gLjkiPb1OnEvfvt8/tracks', headers=header,
                                  params=params)
-        self.q.insert(0, song)
         song_id = song.split(':')[-1]
-        song_data = requests.get(f'https://api.spotify.com/v1/tracks/{song_id}', headers=self.header).text
-        playback_data = requests.get('https://api.spotify.com/v1/me/player', headers=self.header).text
-        return playback_data
+        song_data = json.loads(requests.get(f'https://api.spotify.com/v1/tracks/{song_id}', headers=header).text)
+
+        self.header = header
+        self.display(song_data)
+        return self.queue()
 
     def display(self, song_data):
         print(song_data)
 
     def play(self):
-        if self.playing:
-            print(self.q)
-            return {'swager': 'swagger?'}
-        song = self.q.pop()
-        print('reached here')
-        print(song)
-        data = '{"uris":["' + song + '"]}'
+        print('reached')
+        print(self.q)
+        if self.q == 0:
+            requests.put('https://api.spotify.com/v1/me/player/shuffle?state=true', headers=self.header)
+            party_playlist = 'spotify:playlist:5xu64XCQFMAZJG20Lv1y7t'
+            data = '{"context_uri":"' + party_playlist + '"}'
+            response = requests.put('https://api.spotify.com/v1/me/player/play', headers=self.header, data=data)
+            self.timer.cancel()
+            self.timer = threading.Timer(0, lambda: print('Timer started'))
 
-        response = requests.put('https://api.spotify.com/v1/me/player/play', headers=self.header, data=data)
+        else:
+            requests.put('https://api.spotify.com/v1/me/player/shuffle?state=false', headers=self.header)
+            items = json.loads(requests.get('https://api.spotify.com/v1/playlists/2CbUu1gLjkiPb1OnEvfvt8/tracks', headers=self.header).text)
+            items = len(items['items'])
+            print(items)
+            queue_playlist = 'spotify:playlist:2CbUu1gLjkiPb1OnEvfvt8'
+            data = '{"context_uri":"' + queue_playlist + '", "offset": {"position": ' + str(items - self.q) + '}}'
 
-        if len(self.q) > 0:
-            data = dict(json.loads(response.text))
-            self.timer = threading.Timer(10, self.queue)
+            response = requests.put('https://api.spotify.com/v1/me/player/play', headers=self.header, data=data)
 
-        print(response, response.text)
+            self.q -= 1
+            print(response, response.text)
+        self.timer.cancel()
+        self.queue()
 
     def update(self, header):
         self.header = header
         self.updated = True
+
+    def wait(self, time: int):
+        self.timer = threading.Timer(time, self.play)
+        self.timer.start()
+
+    def next(self):
+        self.timer.cancel()
+        self.timer = threading.Timer(0, lambda: print('fake timer'))
+        self.play()
