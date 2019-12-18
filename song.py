8d4cb242b61fb6a65a626e5fe1fbc1b4c4bccee7
Andy Kamath <andy@andykamath.com>
2019-12-18 09:29:40 -0500

    stuff

--- song.py
+++ song.py
@@ -14,15 +14,19 @@ TODO:
 class Song:
     def __init__(self):
         self.q = 0
-        self.timer = threading.Timer(20000, lambda: print('Timer started'))
+        self.timer = threading.Timer(0, self.play)
 
+    '''
+    If nothing is playing, it plays the song in the queue. Otherwise, it starts a timer to wait until the song is done 
+    '''
     def queue(self):
-        playback_data: dict = self.display()
-        if playback_data == '-1':
-            self.play()
-        else:
+        try:
+            playback_data: dict = self.display()
             self.wait((playback_data['item']['duration_ms'] - playback_data['progress_ms']) // 1000)
-        print(playback_data)
+            print(playback_data)
+        except:
+            self.play()
+
 
     def add(self, song, header):
         self.q += 1
@@ -52,15 +56,13 @@ class Song:
             return "-1"
 
     def play(self):
-        print('reached')
+        print(f'Playing {self.q} songs next')
         print(self.q)
         if self.q == 0:
             requests.put('https://api.spotify.com/v1/me/player/shuffle?state=true', headers=self.header)
             party_playlist = 'spotify:playlist:5xu64XCQFMAZJG20Lv1y7t'
             data = '{"context_uri":"' + party_playlist + '"}'
             response = requests.put('https://api.spotify.com/v1/me/player/play', headers=self.header, data=data)
-            self.timer.cancel()
-            self.timer = threading.Timer(0, lambda: print('Timer started'))
 
         else:
             requests.put('https://api.spotify.com/v1/me/player/shuffle?state=false', headers=self.header)
@@ -74,6 +76,7 @@ class Song:
 
             self.q -= 1
             print(response, response.text)
+
         self.timer.cancel()
         self.queue()
 
@@ -87,7 +90,6 @@ class Song:
 
     def next(self):
         self.timer.cancel()
-        self.timer = threading.Timer(0, lambda: print('fake timer'))
         self.play()
 
     def search(self, song, access):
